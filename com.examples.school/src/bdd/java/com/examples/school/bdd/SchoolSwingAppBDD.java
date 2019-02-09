a5451a212ec20330801069a5c14988c3a2b065a7
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-02-09 16:21:36 +0100

    step for database

--- com.examples.school/src/bdd/java/com/examples/school/bdd/SchoolSwingAppBDD.java
+++ com.examples.school/src/bdd/java/com/examples/school/bdd/SchoolSwingAppBDD.java
@@ -5,6 +5,16 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
+/**
+ * Communicates with a MongoDB server on localhost; start MongoDB with Docker with
+ * 
+ * <pre>
+ * docker run -p 27017:27017 --rm mongo:4.2.3
+ * </pre>
+ * 
+ * @author Lorenzo Bettini
+ *
+ */
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/bdd/resources", monochrome = true)
 public class SchoolSwingAppBDD {
--- com.examples.school/src/bdd/java/com/examples/school/bdd/steps/SchoolSwingAppSteps.java
+++ com.examples.school/src/bdd/java/com/examples/school/bdd/steps/SchoolSwingAppSteps.java
@@ -1,5 +1,11 @@
 package com.examples.school.bdd.steps;
 
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -7,10 +13,32 @@ import io.cucumber.java.en.When;
 
 public class SchoolSwingAppSteps {
 
+	private static final String DB_NAME = "test-db";
+	private static final String COLLECTION_NAME = "test-collection";
+
+	private MongoClient mongoClient;
+
+	@Before
+	public void setUp() {
+		mongoClient = new MongoClient();
+		// always start with an empty database
+		mongoClient.getDatabase(DB_NAME).drop();
+	}
+
+	@After
+	public void tearDown() {
+		mongoClient.close();
+	}
+
 	@Given("The database contains a student with id {string} and name {string}")
-	public void the_database_contains_a_student_with_id_and_name(String string, String string2) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+	public void the_database_contains_a_student_with_id_and_name(String id, String name) {
+		mongoClient
+			.getDatabase(DB_NAME)
+			.getCollection(COLLECTION_NAME)
+			.insertOne(
+				new Document()
+					.append("id", id)
+					.append("name", name));
 	}
 
 	@When("The Student View is shown")
