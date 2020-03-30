LorenzoBettini/sonarqube-example

separate step files

com.examples.school/src/bdd/java/com/examples/school/bdd/steps/DatabaseSteps.java
@@ -1,0 +1,46 @@
+package com.examples.school.bdd.steps;
+
+import java.util.List;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+
+public class DatabaseSteps {
+
+	static final String DB_NAME = "test-db";
+	static final String COLLECTION_NAME = "test-collection";
+
+	private MongoClient mongoClient;
+
+	@Before
+	public void setUp() {
+		mongoClient = new MongoClient();
+		// always start with an empty database
+		mongoClient.getDatabase(DB_NAME).drop();
+	}
+
+	@After
+	public void tearDown() {
+		mongoClient.close();
+	}
+
+	@Given("The database contains the students with the following values")
+	public void the_database_contains_the_students_with_the_following_values(
+			List<List<String>> values) {
+		values.forEach(
+			v -> mongoClient
+				.getDatabase(DB_NAME)
+				.getCollection(COLLECTION_NAME)
+				.insertOne(
+					new Document()
+						.append("id", v.get(0))
+						.append("name", v.get(1)))
+		);
+	}
+
+}

LorenzoBettini/app-ui-docker-mongo-example

com.examples.school/src/bdd/java/com/examples/school/bdd/steps/DatabaseSteps.java
@@ -1,0 +1,46 @@
+package com.examples.school.bdd.steps;
+
+import java.util.List;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+
+public class DatabaseSteps {
+
+	static final String DB_NAME = "test-db";
+	static final String COLLECTION_NAME = "test-collection";
+
+	private MongoClient mongoClient;
+
+	@Before
+	public void setUp() {
+		mongoClient = new MongoClient();
+		// always start with an empty database
+		mongoClient.getDatabase(DB_NAME).drop();
+	}
+
+	@After
+	public void tearDown() {
+		mongoClient.close();
+	}
+
+	@Given("The database contains the students with the following values")
+	public void the_database_contains_the_students_with_the_following_values(
+			List<List<String>> values) {
+		values.forEach(
+			v -> mongoClient
+				.getDatabase(DB_NAME)
+				.getCollection(COLLECTION_NAME)
+				.insertOne(
+					new Document()
+						.append("id", v.get(0))
+						.append("name", v.get(1)))
+		);
+	}
+
+}